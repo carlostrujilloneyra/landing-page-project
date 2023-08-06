057f75f2b2a827d23eaed3a492c85377aa90cfb0
Carlos Trujillo <augusto21_20@hotmail.com>
2023-08-05 20:09:05 -0500

    Creando directorios, componentes

---
+++ src/landing/data/social-networks.ts
@@ -0,0 +1,18 @@
+
+export const arraySocialNetworks = [
+	{
+		url: '/assets/social-networks/icon-facebook.svg'
+	},
+
+	{
+		url: '/assets/social-networks/icon-pinterest.svg'
+	},
+
+	{
+		url: '/assets/social-networks/icon-twitter.svg'
+	},
+
+	{
+		url: '/assets/social-networks/icon-linkedin.svg'
+	}
+];
---
+++ src/landing/interfaces/home-info-main.interface.ts
@@ -0,0 +1,6 @@
+
+export interface InfoMain{
+	image: string,
+	title: string,
+	description: string
+}
