f871ef78c53bb94b5ee05a03779d25e3739b78f8
Carlos Trujillo <augusto21_20@hotmail.com>
2023-08-16 02:13:51 -0500

    Agregando animaciones

--- src/landing/components/about/directors/DirectorList.tsx
+++ src/landing/components/about/directors/DirectorList.tsx
@@ -1,13 +1,22 @@
 import { directors } from '../../../data/directors';
 import { DirectorCard } from './components';
-import { motion } from 'framer-motion';
+import { cubicBezier, motion } from 'framer-motion';
 import './DirectorList.css';
 
 export const DirectorList = () => {
 	return (
 		<>
 			<main className="main-about-container container">
-				<h2 className='about-directors__subtitle'>Meet the directors</h2>
+				<motion.div
+					initial={{ opacity: 0, y: -400 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{
+						duration: 0.8,
+						ease: cubicBezier(0.6, 0.01, -0.05, 0.75)
+					}}
+				>
+					<h2 className='about-directors__subtitle'>Meet the directors</h2>
+				</motion.div>
 				<motion.div
 					className='about-container__directors'
 					initial={{ opacity: 0 }}
